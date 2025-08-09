2a227e962bbd79e7827127f9d56c97a3e40bfa8e
vedbulsara04 <vedbulsara7@gmail.com>
2025-08-09 17:20:11 +0530

    func & control structures

--- go_fast_tutorial/tutorial_3/main.go
+++ go_fast_tutorial/tutorial_3/main.go
@@ -4,10 +4,27 @@ import "fmt"
 
 func main(){
 	printMe()
+	
+	var printValue string = "Hello Ved!"
+	printMe2(printValue)
+	
+	var numerator int = 11
+	var denominator int = 2
+	var result int = intDivision(numerator, denominator)
+	fmt.Println("intDivision result: ", result)
 }
 
 func printMe(){
-	fmt.Println("Hello World")
+	fmt.Println("Go says hi!")
+}
+
+func printMe2(printValue string){
+	fmt.Println(printValue)
+}
+
+func intDivision(numerator int, denominator int) int{
+	var result int = numerator/denominator
+	return result
 }
 
 
