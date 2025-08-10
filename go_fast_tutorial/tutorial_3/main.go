95798e9409e6d6821899f3c2932cc7681e97ecf7
vedbulsara04 <vedbulsara7@gmail.com>
2025-08-10 22:37:57 +0530

    control structures

--- go_fast_tutorial/tutorial_3/main.go
+++ go_fast_tutorial/tutorial_3/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 func main(){
 	printMe()
@@ -9,10 +12,17 @@ func main(){
 	printMe2(printValue)
 	
 	var numerator int = 11
-	var denominator int = 2
-	var result, remainder int = intDivision(numerator, denominator)
-	// fmt.Println("intDivision result: ", result)
-	fmt.Printf("The result of integer division is %v with remainder as %v", result, remainder)
+	var denominator int = 0
+	var result, remainder, err = intDivision(numerator, denominator)
+	// check if error encountered:
+	if err!=nil{
+		fmt.Printf(err.Error())
+	}else if remainder == 0{
+		fmt.Printf("The result of integer division is %v", result)
+	}else{
+		// fmt.Println("intDivision result: ", result)
+		fmt.Printf("The result of integer division is %v with remainder as %v", result, remainder)
+	}
 }
 
 func printMe(){
@@ -24,9 +34,13 @@ func printMe2(printValue string){
 }
 
 func intDivision(numerator int, denominator int) (int, int) {
+	var err error
+	if denominator==0{
+		err = errors.New("Cannot divide by zero!")
+		return 0, 0, err
+	}
 	var result int = numerator/denominator
 	var remainder int = numerator%denominator
-	return result, remainder
+	return result, remainder, err
 }
-
 
