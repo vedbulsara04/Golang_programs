12fcf205c117ca553730521395f17a9ff0921a77
vedbulsara04 <vedbulsara7@gmail.com>
2025-08-11 23:00:35 +0530

    arrays

--- go_fast_tutorial/tutorial_4/main.go
+++ go_fast_tutorial/tutorial_4/main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main(){
-	var intArr [3]int32
-	intArr[1] = 123
+	var intArr [3]int32 = [3]int32{1,2,3}
+	// intArr[1] = 123
 	fmt.Println(intArr[0])
 	fmt.Println(intArr[1:3])
+	fmt.Println(intArr)
 }
 
