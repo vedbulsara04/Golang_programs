1db00ffe329081794e5a7699c620e27ae200e002
vedbulsara04 <vedbulsara7@gmail.com>
2025-08-13 00:00:31 +0530

    slice

--- go_fast_tutorial/tutorial_4/main.go
+++ go_fast_tutorial/tutorial_4/main.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main(){
+	// Array
 	var intArr [3]int32 = [3]int32{1,2,3}
 	// intArr[1] = 123
 	fmt.Println(intArr[0])
 	fmt.Println(intArr[1:3])
 	fmt.Println(intArr)
+	
+	// Slice
+	var intSlice []int32 = []int32{4,5,6}
+	fmt.Println(intSlice)	
+	
 }
 
