a42806a858d869fe7720c2610c6474cbedc1d1ee
vedbulsara04 <vedbulsara7@gmail.com>
2025-08-16 13:22:52 +0530

    strings & runes

---
+++ go_fast_tutorial/tutorial_5/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var myString = "résumé"
+	var indexed = myString[0]
+	
+	fmt.Printf("%v, %T\n", indexed, indexed)
+	
+	for i, v := range myString{
+		fmt.Println(i, v)
+	}
+	
+	
+}
+
