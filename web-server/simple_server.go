27a15238cfe07c528a3de84aaf6c5b4de8f45981
Ved Bulsara <vedbulsara7@gmail.com>
2025-08-18 22:12:11 +0530

    web-server

---
+++ web-server/simple_server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Server listening on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
