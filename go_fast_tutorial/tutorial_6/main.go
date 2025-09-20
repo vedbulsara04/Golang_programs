5d1599a49f600c8d4d680748e4e6bf96b936b8c8
vedbulsara04 <vedbulsara7@gmail.com>
2025-09-20 16:05:42 +0530

    struct

--- go_fast_tutorial/tutorial_6/main.go
+++ go_fast_tutorial/tutorial_6/main.go
@@ -6,7 +6,7 @@ import "fmt"
 type icEngine struct {
 	kml       uint8 //mileage | o.p(undeclared values): 0 [uint8]
 	litres    uint8 //litres of fuel left | o.p(undeclared values): 0 [uint8]
-	ownerInfo owner
+	owner
 }
 
 type owner struct {
@@ -16,12 +16,13 @@ type owner struct {
 func main() {
 	//var myICE icEngine
 	//var myICE icEngine = icEngine{kml:25, litres:15}
-	//var myICE icEngine = icEngine{25, 15}
-	var myICE icEngine
+	var myICE icEngine = icEngine{25, 15, owner{"Ved"}}
+	
+	/* var myICE icEngine
 	myICE.kml = 20
 	myICE.litres = 9
-	myICE.ownerInfo.name = "Ved"
+	myICE.ownerInfo.name = "Ved" */
 
-	fmt.Println(myICE.kml, myICE.litres, myICE.ownerInfo.name)
-
+	fmt.Println(myICE.kml, myICE.litres, myICE.name)
+	
 }
