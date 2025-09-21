029e98390f9b6835ef0398eb30c5856d65a21e97
vedbulsara04 <vedbulsara7@gmail.com>
2025-09-21 19:42:56 +0530

    pointers

--- go_fast_tutorial/tutorial_7/pointers1.go
+++ go_fast_tutorial/tutorial_7/pointers1.go
@@ -9,6 +9,9 @@ func main(){
 	fmt.Printf("The value of p is: %v", i)
 	
 	// *p = 10  //reference the value of pointer
-	
+	p = &i // & means we want memory address of the var and not its value
+	*p = 1 // if value of p is changed, value of i is also changed
+	fmt.Printf("The value p points to is: %v", *p)
+	fmt.Printf("The value of p is: %v", i)
 }
 
