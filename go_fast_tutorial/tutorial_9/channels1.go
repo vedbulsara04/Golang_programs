73bf35e15a0ccd876fb709b322ebd75c7c83151c
vedbulsara04 <vedbulsara7@gmail.com>
2025-09-27 18:29:57 +0530

    channels

---
+++ go_fast_tutorial/tutorial_9/channels1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var c = make(chan int)
+	go process(c)
+	fmt.Println(<- c)
+}
+
+
+func process(c chan int){
+	for i:= 0; i<5; i++{
+		c <- i		
+	}
+}
+
