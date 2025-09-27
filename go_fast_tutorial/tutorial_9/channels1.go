1f649c203459be85c6a266726247f4b911131adb
vedbulsara04 <vedbulsara7@gmail.com>
2025-09-27 18:43:05 +0530

    channels

--- go_fast_tutorial/tutorial_9/channels1.go
+++ go_fast_tutorial/tutorial_9/channels1.go
@@ -7,8 +7,8 @@ import (
 func main(){
 	var c = make(chan int)
 	go process(c)
-	for i:=0; i<5; i++{
-		fmt.Println(<- c)
+	for i:= range c{
+		fmt.Println(i)
 	}
 }
 
@@ -19,6 +19,7 @@ func process(c chan int){
 */
 
 func process(c chan int){
+	defer close(c)
 	for i:=0; i<5; i++{
 		c <- i		
 	}
