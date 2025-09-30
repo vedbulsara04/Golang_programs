4e125d6a3a76b5587f5a6d858bf4bac74eccd1f4
vedbulsara04 <vedbulsara7@gmail.com>
2025-09-30 21:47:54 +0530

    type-conversion

---
+++ 1-learning-go-oreilly/5-explicit-type-conversion/intbyte.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var x int = 10
+	var y byte = 100 //byte = uint8
+	
+	var sum1 byte = byte(x) + y
+	var sum2 int = int(x) + y
+	
+	fmt.Printf()
+
+}
+
