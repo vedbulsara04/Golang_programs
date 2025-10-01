1efe8d7b7464f181d086569de7095062e92ae70b
vedbulsara04 <vedbulsara7@gmail.com>
2025-10-01 21:42:13 +0530

    variables

---
+++ 1-learning-go-oreilly/4-variables/type_inference.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name = "John Doe"
+	var age = 40
+	var temperature = 98.6
+	
+	fmt.Printf("datatype of name: %T, datatype of age: %T, datatype of temperature: %T", name, age, temperature)
+}
+
