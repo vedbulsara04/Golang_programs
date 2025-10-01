37ae0704feb2b0f99bff190916e39d75d27c0acd
vedbulsara04 <vedbulsara7@gmail.com>
2025-10-01 22:07:00 +0530

    zero-values

---
+++ 1-learning-go-oreilly/4-variables/zero_values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	var i int
+	var f float64
+	var b bool
+	var s string
+	var p *int // nil(pointer)
+	
+	fmt.Printf("int: %v\n", i)
+	fmt.Printf("float64: %v\n", f)
+	fmt.Printf("bool: %v\n", b)
+	fmt.Printf("string: %v\n", s)
+	fmt.Printf("pointer: %v\n", p)
+}
+
