316191abb1f411b73cc4a60674cbc7eb036ca507
vedbulsara04 <vedbulsara7@gmail.com>
2025-10-11 19:57:45 +0530

    slices

---
+++ 1-learning-go-oreilly/8-composite-data-types/slices1.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	// slice declaration
+	var s1 []int	// nil slice
+	fmt.Printf("s1: %v\n", s1)
+	s2 := []int{}	// empty slice (not nill)
+	fmt.Printf("s2: %v\n", s2)
+	s3 := []int{1, 2, 3}	// slice literal
+	fmt.Printf("s3: %v\n", s3)
+	
+	// using make
+	s4 := make([]int, 5)	// length 5, capacity 5
+	fmt.Printf("s4: %v\n", s4)
+	s5 := make([]int, 3, 5)	// length 3, capacity 5 [0 0 0]
+	fmt.Printf("s5: %v\n", s5)
+	
+	// Appending elements
+	s3 = append(s3, 4, 5, 6)
+	fmt.Printf("Appended s3: %v\n", s3)
+}
+
