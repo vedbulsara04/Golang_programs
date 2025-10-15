6f6cb7d43c3db70dd4542c592f4830a330e8b3af
vedbulsara04 <vedbulsara7@gmail.com>
2025-10-15 19:57:39 +0530

    maps

--- 1-learning-go-oreilly/8-composite-data-types/maps1.go
+++ 1-learning-go-oreilly/8-composite-data-types/maps1.go
@@ -21,6 +21,25 @@ func main(){
 	m4 := make(map[string]int)
 	m4["Charlie"] = 35
 	fmt.Printf("m4: %v\n", m4)
-
+	
+	// Adding/Updating elements
+	m3["Charlie"] = 35	// adding
+	fmt.Printf("m3: %v\n", m3)
+	m3["Alice"] = 26	// updating
+	fmt.Printf("m3: %v\n"e, m3)
+	
+	// accessing elements
+	age := m3["Alice"]
+	fmt.Printf("Alice's age: %v\n", age)	// 26
+	
+	// check if key exists
+	age, exists := m3["Eve"]
+	if exists {
+		fmt.Println("Age: ", age)
+	} else {
+		fmt.Println("Key not found!)
+	}
+	
+	// deleting elements
 }
 		
