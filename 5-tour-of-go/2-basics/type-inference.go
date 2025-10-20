6db7da9ba92ecd39dcb1bf3ef97aae4529982aa7
vedbulsara04 <vedbulsara7@gmail.com>
2025-10-20 21:45:24 +0530

    baics

---
+++ 5-tour-of-go/2-basics/type-inference.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 45
+	fmt.Printf("var v is of type: %v\n", v)
+}
---
+++ 5-tour-of-go/2-basics/zero-values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var s string
+	var b bool
+	var f float32
+
+	fmt.Printf("%v, %v, %v, %v\n", i, s, b, f)
+}
